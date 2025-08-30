c2aa1dcc2e4c8f1404f2857a8ad9112e5cda508b
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-08-30 06:56:45 +0100

    Create settings.py

---
+++ alx_travel_app/alx_travel_app/settings.py
@@ -0,0 +1,15 @@
+# Celery settings
+CELERY_BROKER_URL = 'amqp://localhost'   # RabbitMQ broker
+CELERY_RESULT_BACKEND = 'rpc://'         # store task results
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+# Email backend (example with console backend for dev)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'rasheedbello45@gmail.com'
+EMAIL_HOST_PASSWORD = '08063103228'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
